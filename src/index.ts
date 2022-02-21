a1ffb7689947d62e128a12831fc0af6cd14348dc
Aneta Suns <suns.echoes@gmail.com>
2022-02-21 12:47:37 +0100

    Move TS type declarations to peer dependency

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+/// <reference types="@suns-echoes/ts-types" />
+
 import { FunctionUtils } from './function-utils';
 
 export default FunctionUtils;
--- src/types.d.ts
+++
@@ -1,7 +0,0 @@
-declare interface AnyFunction extends AnyObject {
-	(...args: any[]): any,
-}
-
-declare type ObjectKey = string | symbol;
-
-declare interface AnyObject extends Record<ObjectKey, any> {}
